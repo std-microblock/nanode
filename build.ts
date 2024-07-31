fe7ff99eb3fe1264c79bd2b06344744efc95182c
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2024-07-31 18:40:01 +0800

    1

--- build.ts
+++ build.ts
@@ -5,19 +5,9 @@ import {$} from 'execa';
 
 _main(async () => {
     const version = 'v22.x'
+
     await $`git clone --depth 1 --branch ${version} https://github.com/nodejs/node`
     process.chdir('node')
-
-    writeFileSync('out.test', 'test')
-    console.log(process.env.GITHUB_TOKEN)
-    await createOrUpdateRelease({
-        tag: version,
-        releaseName: version,
-        releaseNotes: 'Upload',
-        token: process.env.GITHUB_TOKEN!,
-        upload_file_name: 'node.exe',
-        upload_file_path: 'out.test'
-    })
 
     if (process.platform === 'win32') {
         await $`.\\vcbuild small-icu`
