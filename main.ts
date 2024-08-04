58b236ffc7ceed1cddbc59e576a6ffd9af34e582
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2024-08-04 19:09:49 +0800

    refactor: parallel

--- main.ts
+++ main.ts
@@ -4,20 +4,7 @@ import { buildAndUploadNanodeAll } from "./build.js";
 process.env.NODE_DEBUG = 'execa'
 
 _main(async () => {
-    const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN }).rest
-    const targetBranchesList = ['v22.x', 'v21.x', 'v20.x', 'v19.x', 'v18.x', 'v17.x', 'v16.x']
-
-    for (const targetBranch of targetBranchesList) {
-        console.log('Building nanode', targetBranch)
-        await buildAndUploadNanodeAll(targetBranch).catch(e => {
-            console.error('Failed to build nanode', targetBranch, e)
-            octokit.repos.createRelease({
-                owner: 'MicroCBer',
-                repo: 'nanode',
-                tag_name: targetBranch,
-                name: targetBranch,
-                body: `BUILD FAILED at ${new Date().toISOString()}`
-            }).catch(e => { })
-        })
-    }
+    const targetBranch = process.argv[2] || 'v18.x'
+    console.log('Building nanode', targetBranch)
+    await buildAndUploadNanodeAll(targetBranch)
 })
