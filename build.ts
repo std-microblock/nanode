573a81b8347d03a318fab8f94ade50addfc8f3e0
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2024-08-04 15:08:11 +0800

    feat: v8 patches

--- build.ts
+++ build.ts
@@ -1,19 +1,34 @@
 import { writeFileSync } from "fs";
 import { createOrUpdateRelease } from "./github.js";
-import { _main } from "./utilities.js";
+import { _main, patchFile } from "./utilities.js";
 import { $ } from 'execa';
 import { rmdir } from "fs/promises";
 import { minifyUndici } from "./minify-undici.js";
 
 
 export const buildAndUploadNanodeAll = async (version = 'v18.x') => {
-    await buildAndUploadNanode(version, { icu_mode: 'none' })
-    await buildAndUploadNanode(version, { icu_mode: 'system' })
+    for (const target_arch of ['x64', 'arm64', 'x86'] as const) {
+        // Skip x86 on Windows, as it's not supported anymore
+        if (target_arch === 'x86' && process.platform === 'win32') continue;
+
+        await buildAndUploadNanode(version, { icu_mode: 'none', v8_opts: true, target_arch })
+        await buildAndUploadNanode(version, { icu_mode: 'none', v8_opts: true, target_arch, no_jit: true })
+        await buildAndUploadNanode(version, { icu_mode: 'none', target_arch })
+        if (process.platform !== 'win32') {
+            await buildAndUploadNanode(version, { icu_mode: 'system', target_arch })
+        }
+    }
 }
 export const buildAndUploadNanode = async (version = 'v18.x', {
-    icu_mode = 'full'
+    icu_mode = 'full',
+    v8_opts = false,
+    target_arch = 'x64',
+    no_jit = false
 }: {
-    icu_mode?: 'full' | 'small' | 'system' | 'none'
+    icu_mode?: 'full' | 'small' | 'system' | 'none',
+    v8_opts?: boolean,
+    target_arch?: 'x64' | 'arm64' | 'x86',
+    no_jit?: boolean
 }) => {
     await $`git clone --depth 1 --branch ${version} https://github.com/nodejs/node`
     process.chdir('node')
@@ -22,21 +37,51 @@ export const buildAndUploadNanode = async (version = 'v18.x', {
         console.error('Failed to minify undici', e)
     })
 
+    const buildName = `nanode-${version}-icu_${icu_mode}${v8_opts ? '-v8_opts' : ''}${no_jit ? '-nojit' : ''}`;
     try {
+        if (v8_opts)
+            await patchFile('configure.py', code => {
+                code = code.replaceAll('options.v8_disable_object_print', 'True');
+                // code = code.replaceAll('options.v8_enable_pointer_compression', 'False');
+                code = code.replaceAll('options.without_inspector', 'True');
+                code = code.replaceAll('options.v8_enable_i18n_support', 'False');
+                return code
+            })
+
+        if (no_jit)
+            await patchFile('configure.py', code => {
+                code = code.replaceAll('options.v8_lite_mode', 'True');
+                return code
+            })
+
+        await patchFile('configure.py', code => {
+            code = code.replaceAll('options.without_npm', 'True');
+            code = code.replaceAll('options.without_corepack', 'True');
+            code = code.replaceAll('options.without_amaro', 'True');
+
+            return code
+        })
+
+
         if (process.platform === 'win32') {
-            await $`cmd /c vcbuild.bat none`
+            const winIcuArg = {
+                full: 'full-icu',
+                small: 'small-icu',
+                none: 'intl-none'
+            }
+            await $`cmd /c vcbuild.bat ${target_arch} ${winIcuArg[icu_mode]}`
 
             await createOrUpdateRelease({
                 tag: version,
                 releaseName: version,
                 releaseNotes: 'Upload',
                 token: process.env.GITHUB_TOKEN!,
-                upload_file_name: `nanode-${version}-icu_${icu_mode}.exe`,
+                upload_file_name: `${buildName}.exe`,
                 upload_file_path: 'out\\Release\\node.exe'
             })
 
         } else {
-            await $`./configure --with-intl=none`
+            await $`./configure --with-intl=${icu_mode === 'none' ? icu_mode : `${icu_mode}-icu`}`
             await $`make -j4`
 
             await createOrUpdateRelease({
@@ -44,7 +89,7 @@ export const buildAndUploadNanode = async (version = 'v18.x', {
                 releaseName: version,
                 releaseNotes: 'Upload',
                 token: process.env.GITHUB_TOKEN!,
-                upload_file_name: 'node',
+                upload_file_name: buildName,
                 upload_file_path: 'out/Release/node'
             })
         }
--- utilities.ts
+++ utilities.ts
@@ -1,4 +1,10 @@
+import { readFile, writeFile } from "fs/promises"
 
 export const _main = (func) => {
     func()
+}
+
+export const patchFile = async (file: string, patch: ((orig: string)=>string)) => {
+    const original = await readFile(file, 'utf-8')
+    await writeFile(file, patch(original))
 }
