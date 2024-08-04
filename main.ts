65dc22c600f74608718fde279dfa45e973040d8e
MicroBlock <66859419+MicroCBer@users.noreply.github.com>
2024-08-04 10:35:46 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,14 @@ import { buildAndUploadNanode } from "./build.js";
 
 _main(async () => {
     const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN }).rest
-
-    const { data: releaseList } = await octokit.repos.listReleases({
-        owner: 'MicroCBer',
-        repo: 'nanode'
-    })
-
     const targetBranchesList = ['v22.x', 'v21.x', 'v20.x', 'v19.x', 'v18.x', 'v17.x', 'v16.x', 'v15.x', 'v14.x', 'v13.x', 'v12.x', 'v11.x', 'v10.x', 'v9.x', 'v8.x']
 
     for (const targetBranch of targetBranchesList) {
+        const { data: releaseList } = await octokit.repos.listReleases({
+            owner: 'MicroCBer',
+            repo: 'nanode'
+        })
+
         const release = releaseList.find(release => release.tag_name === targetBranch)
         if (!release) {
             console.log('Building nanode', targetBranch)
